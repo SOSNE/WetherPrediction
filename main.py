fa791b020e4ad1082829ecd4a678226e7027b301
SOSNE <tomjansan@gmail.com>
2025-05-29 22:07:13 +0200

    Added more dada processing and simple machine learning with tensorflow.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import tensorflow as tf
+from pyasn1_modules.rfc5755 import Target
 
 columns = ['date', 'tavg', 'tmin', 'tmax', 'prcp', 'snow',
            'wdir', 'wspd', 'wpgt', 'pres', 'tsun', 'coco']
@@ -12,5 +15,24 @@ df['snow'] = df['snow'].fillna(0)
 df['prcp'] = df['prcp'].fillna(0)
 df['wpgt'] = df['wpgt'].ffill()
 
-print(df.isnull().sum())
-# print(df)
+
+def prepare_data(data):
+    _input, target = [], []
+    for i in range(len(data)-7):
+        _input.append(data[i:i+7][:, 1:])
+        target.append(data[i+7][1:])
+    return np.array(_input, dtype=np.float64), np.array(target, dtype=np.float64)
+
+_input, target = prepare_data(df.to_numpy())
+
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Input(shape=(7, 9)),
+  tf.keras.layers.LSTM(64, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(9)
+])
+
+model.compile(optimizer='adam', loss='mse')
+
+model.fit(_input, target, epochs=1000)
