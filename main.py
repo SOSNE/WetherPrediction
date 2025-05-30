a89d5660e952eda95ecea01d13ae4c800339837e
SOSNE <tomjansan@gmail.com>
2025-05-30 20:02:46 +0200

    Upgrade in training model diagram.

--- main.py
+++ main.py
@@ -35,11 +35,14 @@ target = scaler.fit_transform(target)
 
 model = tf.keras.Sequential([
     tf.keras.layers.Input(shape=(7, 9)),
-    tf.keras.layers.LSTM(64, activation='relu'),
-    tf.keras.layers.Dropout(0.1),
+    tf.keras.layers.LSTM(64, return_sequences=True),
+    tf.keras.layers.LSTM(32),
+    tf.keras.layers.Dense(32, activation='relu'),
     tf.keras.layers.Dense(9)
 ])
 
 model.compile(optimizer='adam', loss='mse')
 
-model.fit(_input, target, epochs=1000)
+model.fit(_input, target, epochs=10)
+
+model.save("models/weather_model_1.h5")
